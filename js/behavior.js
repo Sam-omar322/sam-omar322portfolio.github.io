8feb9568a9b0f743d5d610572855dce8466791ad
sam-omar322 <sam.omar564@gmail.com>
2021-11-19 13:52:38 +0300

    first

---
+++ js/behavior.js
@@ -0,0 +1,20 @@
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 200) {
+        $('#navbar').addClass('noTransparrent');
+    } else {
+        $('#navbar').removeClass('noTransparrent');
+    }
+});
+
+$(document).ready(function() {
+    $('.circle').circleProgress({
+        startAngle: -Math.PI / 2,
+        fill: "#0575e6"
+    }).on("circle-animation-progress", function(event, progress, stepValue){
+        $(this).find('span').html(Math.round(stepValue * 100) + "%");
+    });
+
+    $(function() {
+        $('#contact-form').validate();
+    })
+})
